17bd6119a0e1e8b8fd76ec397655a5ed72006734
AnaSanchezS <33376532+AnaSanchezS@users.noreply.github.com>
2018-04-08 14:21:05 -0500

    Update and rename Fibonacci.java to Login.java

---
+++ src/math/Login.java
@@ -0,0 +1,12 @@
+package math;
+
+public class Login {
+ 
+    public static boolean authenticate(String username, String password) {
+        // hardcoded username and password
+        if (username.equals("Ana") && password.equals("secretPassword")) {
+            return true;
+        }
+        return false;
+    }
+}
